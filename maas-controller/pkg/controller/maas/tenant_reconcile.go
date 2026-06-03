3fd5998c4e79ecfd8cab85e5f6af577850788a41
Wen Zhou <wenzhou@redhat.com>
2026-06-03 10:04:47 +0200

    update: docs and comments
    - fix rebase as well
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- maas-controller/pkg/controller/maas/tenant_reconcile.go
+++ maas-controller/pkg/controller/maas/tenant_reconcile.go
@@ -635,7 +635,7 @@ func (r *TenantReconciler) cleanupTenantResources(ctx context.Context, log logr.
 		},
 		{
 			gvk:       tenantreconcile.GVKCronJob,
-			name:      tenantreconcile.MaaSAPIKeyCleanupCronJobName(tenantID),
+			name:      tenantreconcile.LegacyMaaSAPIKeyCleanupCronJobNameForTenant(tenantID),
 			namespace: appNs,
 		},
 	}
