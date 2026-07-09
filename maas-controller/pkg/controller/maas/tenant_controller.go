16ef303f5e4d79190b96901244bcacf4db68a938
Wen Zhou <wenzhou@redhat.com>
2026-07-09 10:49:43 +0200

    fix: rebase
    - cleanup duplicated RBAC
    - remove image from params.env
    - since now the job is named after tenantID, we need loop over all jobs
      and do the cleanup
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- maas-controller/pkg/controller/maas/tenant_controller.go
+++ maas-controller/pkg/controller/maas/tenant_controller.go
@@ -106,7 +106,6 @@ type TenantReconciler struct {
 // +kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=monitoring.coreos.com,resources=podmonitors;servicemonitors,verbs=get;list;watch;create;patch;delete
 // +kubebuilder:rbac:groups=cert-manager.io,resources=certificates,verbs=get;list;watch;create;patch;delete
-// +kubebuilder:rbac:groups=perses.dev,resources=persesdashboards;persesdatasources,verbs=get;list;watch;create;patch;delete
 
 // clusterroles/clusterrolebindings: TenantReconciler SSA-applies the maas-api and payload-processing-reader
 // ClusterRoles. The API-server escalation check requires the applying SA to already hold every permission those
